9516daa73cf1c65bc8d97779e69d8b4b028254f6
Dario Rodríguez ITelligent <drodriguez@itelligent.es>
2025-06-06 21:25:28 -0400

    Instalados paquetes de EntityFramework Cofigurada la base de datos Creadas las primeras entidades Creados los archivos de persistencia Sincronizada la base de datos

--- DemoRestApi/Program.cs
+++ DemoRestApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//Add Entity Framework Core and SQL Server support
+builder.Services.AddDbContext<Infrastructure.Persistence.SQLServerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// Apply migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<Infrastructure.Persistence.SQLServerContext>();
+    dbContext.Database.EnsureCreated(); // Ensure the database is created
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public virtual Company? Company { get; set; }
+    }
+}
